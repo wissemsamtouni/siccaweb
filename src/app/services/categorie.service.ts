aba21734e94f599478116d863ab6640fc50469c5
wissemsamtouni <sa.wissem.samtouni@gmail.com>
2022-12-02 15:42:21 +0100

    le02/12/2022

--- src/app/services/categorie.service.ts
+++ src/app/services/categorie.service.ts
@@ -15,9 +15,9 @@ export class CategorieService {
     return this.http.post('http://localhost:5000/categorie/addcat', cat)
   }
   deletecategorie(id: any) {
-    return this.http.delete('http://localhost:5000/categorie/delete'+id)
+    return this.http.delete('http://localhost:5000/categorie/delete/'+id)
   }
   updatecategorie(id: any,newcat:any) {
-    return this.http.patch('http://localhost:5000/categorie/update'+id,newcat)
+    return this.http.patch('http://localhost:5000/categorie/update/'+id,newcat)
   }
 }
